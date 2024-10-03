dd477ef78bc13138a3cf730bb9e5d9991ed9f388
Chunchinlee <torresmichaellhance@gmail.com>
2024-10-03 20:12:54 +0800

    new

---
+++ src/Components/FirebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ0PlYIIMJOHh2xueY5XdG4hdLu9aQAu0",
+  authDomain: "fixme-a1b21.firebaseapp.com",
+  databaseURL: "https://fixme-a1b21-default-rtdb.firebaseio.com",
+  projectId: "fixme-a1b21",
+  storageBucket: "fixme-a1b21.appspot.com",
+  messagingSenderId: "522121216989",
+  appId: "1:522121216989:web:4834570c0400d50e856c34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
